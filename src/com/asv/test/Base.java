6be31aba47b839101292094c08191bedad3bf7c5
Sergey Aleksandrov <a_svl@yahoo.com>
2016-06-10 18:05:19 +0300

    minor changes

---
+++ src/com/asv/test/Base.java
@@ -0,0 +1,25 @@
+package com.asv.test;
+
+/**
+ * @author alexandrov
+ * @since 10.06.2016
+ */
+public class Base {
+
+    public static void main(String[] args) {
+        long l1 = 0xFFFFFFFF00000000L;
+        long l2 = 0x00000000FFFFFFFFL;
+        long l3 = 0x0FFFFFFFF0000000L;
+        long l4 = 0x0FFFFFFFFFFFFFFFL;
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println("Binary=" + Long.toBinaryString(Long.MAX_VALUE));
+        System.out.println(Long.toBinaryString(Long.MAX_VALUE).length());
+
+    }
+
+}
