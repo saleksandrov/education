5564864463c8599d542c82b272989de91d1574ef
Sergey Aleksandrov <a_svl@yahoo.com>
2016-06-10 12:43:49 +0300

    minor changes

--- src/com/asv/edu/generics/BoundedTypes.java
+++ src/com/asv/edu/generics/BoundedTypes.java
@@ -18,6 +18,7 @@ public class BoundedTypes {
 
     public static void in(List<? extends Number> src) {
         //src.add(new Integer(1));   // compile time error
+        //src.add(new BigInteger("2"));     // compile time error
     }
 
     public static void out(List<? super Integer> dest) {
