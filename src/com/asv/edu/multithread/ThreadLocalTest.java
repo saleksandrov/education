55fad123278f40e313501e970eb46e6a62eac07e
saleksandrov@gmail.com <gh1579qwe>
2016-06-26 13:26:01 +0300

    Added ThreadLocal test

---
+++ src/com/asv/edu/multithread/ThreadLocalTest.java
@@ -0,0 +1,32 @@
+package com.asv.edu.multithread;
+
+import com.sun.xml.internal.ws.api.server.ThreadLocalContainerResolver;
+
+/**
+ * Created by work on 26.06.2016.
+ */
+public class ThreadLocalTest {
+
+    public static void main(String[] args) {
+
+        TL1.threadLocal.set("Test");
+
+        Object o2 = TL2.threadLocal.get();
+        Object o1 = TL1.threadLocal.get();
+
+        System.out.println("o2 = " + o2);
+        System.out.println("o1 = " + o1);
+
+    }
+
+
+    public static class TL1 {
+        public static ThreadLocal threadLocal = new ThreadLocal();
+    }
+
+    public static class TL2 {
+        public static ThreadLocal threadLocal = new ThreadLocal();
+    }
+
+
+}
