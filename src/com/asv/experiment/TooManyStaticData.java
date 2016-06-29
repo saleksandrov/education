ae1071d518b27a745052768474e1683a4dae52b1
Sergey Aleksandrov <a_svl@yahoo.com>
2016-06-29 21:37:10 +0300

    minor changes

---
+++ src/com/asv/experiment/TooManyStaticData.java
@@ -0,0 +1,58 @@
+package com.asv.experiment;
+
+import com.asv.util.PerformEstimator;
+
+/**
+ * @author alexandrov
+ * @since 29.06.2016
+ */
+public class TooManyStaticData {
+
+    public static final int count = 100_000;
+
+    private static long[] data1;
+    private static long[] data2;
+    private static long[] data3;
+
+    private static StringBuilder dataString = new StringBuilder();
+
+
+    public static void main(String[] args) {
+        PerformEstimator.startEstimation();
+        experimentTwo();
+        PerformEstimator.printEstimationInMillis();
+    }
+
+    private static void experimentTwo() {
+        // OutOfMemoryError Java Heap Space (count = 200_000)
+        for (int i = 0; i < count; i ++) {
+            dataString.append(" This is a test data This is a test data This is a test data This is a test data This is a " +
+                    "test data " +
+                    "This is a test data This is a test data This is a test data This is a test data This is a test data " +
+                    "This is a test data This is a test data This is a test data This is a test data This is a test data " +
+                    "This is a test data This is a test data This is a test data This is a test data This is a test data ");
+        }
+
+        System.out.println("dataString.length in kb = " + dataString.length()/1024);
+    }
+
+    private static void experimentOne() {
+        // OutOfMemoryError Java Heap Space (count = 9_000_000)
+        data1 = new long[count];
+        fillArray(data1);
+
+        data2 = new long[10*count];
+        fillArray(data2);
+
+        data3 = new long[count];
+        fillArray(data3);
+    }
+
+    private static void fillArray(long[] data) {
+        for (int i = 0; i < count; i ++) {
+            data[i] = i;
+        }
+    }
+
+
+}
