c42fd6bf6976946c96e45a8da56672a7cfca8b1f
Sergey Aleksandrov <a_svl@yahoo.com>
2016-06-30 16:16:57 +0300

    minor changes

--- src/com/asv/experiment/TooManyStaticData.java
+++ src/com/asv/experiment/TooManyStaticData.java
@@ -2,13 +2,17 @@ package com.asv.experiment;
 
 import com.asv.util.PerformEstimator;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author alexandrov
  * @since 29.06.2016
  */
 public class TooManyStaticData {
 
-    public static final int count = 100_000;
+    public static final int count = 10_000_000;
 
     private static long[] data1;
     private static long[] data2;
@@ -19,8 +23,19 @@ public class TooManyStaticData {
 
     public static void main(String[] args) {
         PerformEstimator.startEstimation();
-        experimentTwo();
+        experimentThree();
         PerformEstimator.printEstimationInMillis();
+    }
+
+    private static void experimentThree() {
+        // OutOfMemoryError Java Heap Space (count = 10_000_000)
+        BigDecimal bigDecimal;
+        List<BigDecimal> data = new ArrayList<>();
+        for (int i = 0; i < count; i ++) {
+            data.add(new BigDecimal(i));
+        }
+
+        System.out.println("data.size: " + data.size());
     }
 
     private static void experimentTwo() {
