8779426ee6744431d717badc757b7d542ac7fce9
Sergey Aleksandrov <a_svl@yahoo.com>
2016-09-08 18:22:15 +0300

    Added string test

---
+++ src/com/asv/edu/string/Base.java
@@ -0,0 +1,18 @@
+package com.asv.edu.string;
+
+import java.util.Objects;
+
+/**
+ * @author alexandrov
+ * @since 08.09.2016
+ */
+public class Base {
+
+    public static void main(String[] args) {
+        String str = new String("abc");
+
+        // разные ссылки
+        System.out.println(str == "abc");
+    }
+    
+}
