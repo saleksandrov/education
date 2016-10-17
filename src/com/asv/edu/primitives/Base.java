97aa5c11e4af48cccea44ba1834f04e1d100ea6b
Sergey Aleksandrov <a_svl@yahoo.com>
2016-10-17 18:15:10 +0300

    Added test class for Integer

---
+++ src/com/asv/edu/primitives/Base.java
@@ -0,0 +1,17 @@
+package com.asv.edu.primitives;
+
+/**
+ * @author alexandrov
+ * @since 17.10.2016
+ */
+public class Base {
+
+    public static void main(String[] args) {
+        Integer i1 = new Integer(2);
+        Integer i2 = new Integer(2);
+
+        boolean b = i1 == i2;
+        System.out.println("i1==i2 = " + b);
+    }
+
+}
